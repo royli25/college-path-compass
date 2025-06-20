f65596bd3cc3ca162aa284db1a52918de42ca6b4
royli25 <royhyli25@gmail.com>
2025-06-19 18:03:33 -0700

    Update package dependencies and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
+    proxy: {
+      '/functions/v1': {
+        target: 'https://mknqrapsxcylikcljisu.supabase.co',
+        changeOrigin: true,
+      },
+    }
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
